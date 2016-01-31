b8382effed9963c6e339ef173afad6d6827397bb
Jernej Pogačar <pogacar.jernej@gmail.com>
2016-01-31 12:12:12 +0000

    simplify package

--- vue-bootstrap-pagination.js
+++ vue-bootstrap-pagination.js
@@ -1,5 +1,21 @@
 module.exports = {
-    template: require('./pagination.html'),
+    template: '<nav>' +
+        '<ul class="pagination">' +
+            '<li v-if="pagination.current_page > 1">' +
+                '<a href="#" aria-label="Previous" @click.prevent="changePage(pagination.current_page - 1)">' +
+                    '<span aria-hidden="true">&laquo;</span>' +
+                '</a>' +
+            '</li>' +
+            '<li v-for="num in array" :class="{\'active\': num == pagination.current_page}">' +
+                '<a href="#" @click.prevent="changePage(num)">{{ num }}</a>' +
+            '</li>' +
+            '<li v-if="pagination.current_page < pagination.last_page">' +
+                '<a href="#" aria-label="Next" @click.prevent="changePage(pagination.current_page + 1)">' +
+                    '<span aria-hidden="true">&raquo;</span>' +
+                '</a>' +
+            '</li>' +
+        '</ul>' +
+    '</nav>',
     props: {
         pagination: {
             type: Object,
